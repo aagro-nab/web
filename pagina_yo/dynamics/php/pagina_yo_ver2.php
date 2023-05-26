9b138afc95e6557d014d49a6d127ec011a96f808
Ame <322317150@alumno.enp.unam.mx>
2023-05-26 00:52:23 -0600

    feat, fix (css, html) : agrega estilos

--- pagina_yo/dynamics/php/pagina_yo_ver2.php
+++ pagina_yo/dynamics/php/pagina_yo_ver2.php
@@ -4,123 +4,152 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href=".\statics\styles\pagina_yo.css">
+    <link rel="stylesheet" href="..\..\statics\styles\pagina_yo.css">
     <title> Yo </title>
 </head>
 <body>
     <font face="MS UI Gothic">
-    <br>
-    <font size="7" color="#224042"><b> YO </b></font> <br><br>
-    <font size="7" color="#224042"><b><i> Soy Ameyalli Aguilar Rojo, pero me pueden decir Ame :) </i></b></font>
 
-    <br><br><br>
+    <div id="flor-1"> <img src="..\..\statics\media\img\flor.png" width="100"> </div>
+    <div id="flor-2"> <img src="..\..\statics\media\img\flor.png" width="100"> </div>
 
-    <font size="6" color="#224042"><b> Así me veo </b></font> <br><br>
-    <!--mi foto-->
-    <td align="center" valign="middle" height="250"> <img src="..\..\statics\media\img\yo.jpeg" width="360"> </td>
-    <br><br><br>
+    <header>
+        <div id="titulo"> Hola, soy Ame :) </div>
+        <div class="sub" id="inicio"> Esto es un poquito sobre mí </div>
+    </header>
 
-    <font size="6" color="#224042"><b> Mis intereses :P </b></font>
-    <!--<font size="4">-->
-    <p>
-    Me interesan muchas cosas, y cambian con el tiempo.
-    </p>
-    <p> Me gusta mucho la <b>música</b>, y me gustaría aprender a producir. <br> Ya pude hacer mi primer sample, y la verdad 
-    siento yo me quedó bastante bien ^^. </p>
-    <p> También amo el <b>cine</b>. <br> Me gusta mucho ver películas cuando está lloviendo, son de mis momentos favoritos. <br>
-    Me gustaría hacer una serie o película, pero siento que eso es un poco más difícil xd. </p>
-    <p> Otra cosa que hago es <b>dibujar</b>. <br> Probablemente de esas tres cosas es de lo que mejor sé hacer, y lo disfruto mucho. </p>
-    <p> Fueron 4 párrafos pero mejor que sobre a que falte. </p>
+    <section id="fotito">
+        <!--mi foto-->
+        <div id="mi_foto"> <a href="https://www.youtube.com/watch?v=xvFZjo5PgG0&ab_channel=Duran"> <img src="..\..\statics\media\img\yo.jpeg" width="320"> </a> </div>
+        <div id="intereses">
+            <div class="sub"> Mis intereses :P </div>
+            <div>
+                <p>
+                Me interesan muchas cosas, y cambian con el tiempo.
+                </p>
+                <p> Me gusta mucho la <b>música</b>, y me gustaría aprender a producir. <br> Ya pude hacer mi primer sample, y la verdad 
+                siento yo me quedó bastante bien ^^. </p>
+                <p> También amo el <b>cine</b>. <br> Me gusta mucho ver películas cuando está lloviendo, son de mis momentos favoritos. <br>
+                Me gustaría hacer una serie o película, pero siento que eso es un poco más difícil xd. </p>
+                <p> Otra cosa que hago es <b>dibujar</b>. <br> Probablemente de esas tres cosas es de lo que mejor sé hacer, y lo disfruto mucho.</p>
+            </div>
+        </div>
+    </section>
 
+    <section id="pelis">
+        <!--mi foto-->
+        <div class="sub_centro"> Mis peliculas favoritas </div>
+        <div id="galeria">
+            <div class="cuadrito">
+                <div class="imagen-peli" > <a href="https://letterboxd.com/film/drive-my-car/"> <img src="..\..\statics\media\img\drive_my_car.jpg" width="200"></a> </div>
+                <div class="textito"> Drive My Car </div>
+            </div>
+            <div class="cuadrito">
+                <div class="imagen-peli"> <a href="https://letterboxd.com/film/little-miss-sunshine/"> <img src="..\..\statics\media\img\little_miss_sunshine.png" width="200"> </a></div>
+                <div class="textito"> Little Miss Sunshine </div>
+            </div>
+            <div class="cuadrito">
+                <div class="imagen-peli"> <a href="https://letterboxd.com/film/your-name/"> <img src="..\..\statics\media\img\your_name.jpg" width="200"> </a> </div>
+                <div class="textito"> Your Name </div> 
+            </div>
+            <div class="cuadrito">
+                <div class="imagen-peli"> <a href="https://letterboxd.com/film/fight-club/"> <img src="..\..\statics\media\img\fight_club.jpg" width="200"> </a> </div>
+                <div class="textito"> Fight Club </div>
+            </div>
+            <div class="cuadrito">
+                <div class="imagen-peli"> <a href="https://letterboxd.com/film/jojo-rabbit/"> <img src="..\..\statics\media\img\jojo_rabbit.jpg" width="200"> </a> </div>
+                <div class="textito"> Jojo Rabbit </div> 
+            </div>
+        </div>
+    </section>
 
-    <font size="6" color="#224042"><b> Mis peliculas favoritas </b></font>
-    <ol>
-        <li> Drive My Car </li>
-        <li> Little Miss Sushine </li>
-        <li> Your Name </li>
-        <li> The Fight Club </li>
-        <li> Spirited Away </li>
-    </ol>
+    <section id="comidas">
+        <div id="lista">
+            <div class="especial"> Mis comidas favoritas </div>
+            <div class="contenido"> 
+                <div class="sub-pequeño"> Pulpo a la gallega </div>
+                <div class="sub-pequeño"> Pasta puttanesca </div>
+                <div class="sub-pequeño"> Tacos de mixiotes </div>
+                <div class="sub-pequeño"> Salpicón </div>
+                <div class="sub-pequeño"> Rámen </div>
+            </div>
+        </div>
+        <div id="lista-2">
+            <div class="especial-2"> Mi top de temas del CW 2023 :O </div>
+            <div class="contenido">
+                <div class="sub-pequeño"> 5. Bases de Datos <br><br> esta cool pero me cuesta trabajo y me intimida</div>
+                <div class="sub-pequeño"> 4. HTML <br><br> no brilla sin php ni css, por eso numero 4</div>
+                <div class="sub-pequeño"> 3. PHP <br><br> amo la libertad que te da a diferencia de c, pero nada completamente nuevo para mi</div>
+                <div class="sub-pequeño"> 2. Git/Github <br><br> no se solo es divertido xd</div>
+                <div class="sub-pequeño"> 1. CSS <br><br> mi lado area 4 combinado con el area 1, no podria pedir mas</div>
+            </div>
+        </div>
+    </section>
 
-    <font size="6" color="#224042"><b> Mis comidas favoritas </b></font>
-    <ul type="disc">
-        <li> Pulpo a la gallega </li>
-        <li> Pasta puttanesca </li>
-        <li> Tacos de mixiotes </li>
-        <li> Salpicón </li>
-        <li> Rámen </li>
-    </ul>    
+    <section id="cancioncitas">
+        <div class="sub"> Mis canciones favoritas del momento </div>
+        <!--mi canción favorita-->
+        <div class="video-completo">
+            <div class="videos-columna">
+                <div class="cuadrito">
+                    <div class="imagen-peli" > <a href="https://www.youtube.com/watch?v=3VGCAPpmXxA&ab_channel=Tentendo-Topic"> <img src="..\..\statics\media\img\estrella_2.png" width="200"> </a></div>
+                    <div class="textito"> <a href="https://www.youtube.com/watch?v=3VGCAPpmXxA&ab_channel=Tentendo-Topic"> Money - Tendendo, honey </a> </div>
+                </div>
+                <div class="cuadrito">
+                    <div class="imagen-peli"> <a href="https://www.youtube.com/watch?v=0UxlvCuBLzA&ab_channel=Acru"> <img src="..\..\statics\media\img\estrella_3.png" width="200"> </a> </div>
+                    <div class="textito"> <a href="https://www.youtube.com/watch?v=0UxlvCuBLzA&ab_channel=Acru"> Abril - ACRU </a> </div>
+                </div>
+            </div>
+            <div class="videos-columna">
+                <div class="cuadrito">
+                    <div class="imagen-peli"> <a href="https://www.youtube.com/watch?v=qm5y_ZCSra4&ab_channel=MacDeMarco-Topic"> <img src="..\..\statics\media\img\estrella_4.png" width="200"> </a> </div>
+                    <div class="textito"> <a href="https://www.youtube.com/watch?v=qm5y_ZCSra4&ab_channel=MacDeMarco-Topic"> Only You - Mac DeMacro </a> </div> 
+                </div>
+                <div class="cuadrito">
+                    <div class="imagen-peli"> <a href="https://www.youtube.com/embed/HfkjnAv_uRE"> <img src="..\..\statics\media\img\estrella.png" width="200"> </a> </div>
+                    <div class="textito"> <a href="https://www.youtube.com/watch?v=HfkjnAv_uRE&ab_channel=CTanganaVEVO"> Muriendo De Envidia - C. Tangana, Eliades Ochoa </a>  </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
-    <font size="6" color="#224042"><b> Mi canción favorita </b></font> <br><br>
-    <!--mi canción favorita-->
-    <a href="https://www.youtube.com/watch?v=ccztRby3FAk"> Car Seat Headrest - "Drunk Drivers/Killer Whales" </a>
-    <br> <br> <br>
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/ccztRby3FAk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    <section id="personajes">
+        <div class="sub"> Mis personajes favoritos </div>
+        <div id="columna-personajes">
+            <div class="tarjeta">
+                <div class="nombre"> <div class="otro-sub-pequeño">Haruhi Fujioka <br><br> de Ouran High School Host Club</div> </div>
+                <div class="descripcion"> <div class="texto-personaje"> Es hermosa y linda y amable e inteligente<br><br>Haruhi cásate conmigo </div> </div>
+                <div class="imagen-personaje"> <img src="..\..\statics\media\img\haruhi.png" height="250"> </div>
+            </div>
+            <div class="tarjeta">
+                <div class="nombre"> <div class="otro-sub-pequeño">Kel <br><br> de Omori</div> </div>
+                <div class="descripcion"> <div class="texto-personaje">Que lindo que es por dios <br> Otro personaje ficticio que me gustaría tener en mi vida <br> Es muy atletico y le gusta el basketball <br> Me gusta su corte de pelo </div></div>
+                <div class="imagen-personaje"> <div class="personaje-individual"><img src="..\..\statics\media\img\kel.png" height="250"> </div></div>
+            </div>
+            <div class="tarjeta">
+                <div class="nombre"> <div class="otro-sub-pequeño"> Waymond Wang<br> <br> de Everything Everywhere All At Once</div> </div>
+                <div class="descripcion"> <div class="texto-personaje"> Por el todavía creo en la humanidad <br>Que lindo ser humano de verdad que sí <br>Pasa desapercibido pero es de los personajes más importantes y significativos de la película </br> La verdad, soy yo </div></div>
+                <div class="imagen-personaje"> <div class="personaje-individual"><img src="..\..\statics\media\img\waymond.png" height="250"> </div></div>
+            </div>
+            <div class="tarjeta">
+                <div class="nombre"> <div class="otro-sub-pequeño">Kaworu Nagisa <br><br> de Neon Genesis Evangelion</div> </div>
+                <div class="descripcion"> <div class="texto-personaje"> Es un literalmente un ser perfecto <br> Amo su cabello gris parece un viejito y si le queda por que es muy sabio <br> Quiero que alguien me quiera como el quiere a Shinji <br> Te amo Kaworu </div></div>
+                <div class="imagen-personaje"> <div class="personaje-individual"><img src="..\..\statics\media\img\kaworu.png" height="250"> </div></div>
+            </div>
+            <div class="tarjeta">
+                <div class="nombre"> <div class="otro-sub-pequeño">Mr. Peanutbutter <br><br> de Bojack Horseman</div> </div>
+                <div class="descripcion"> <div class="texto-personaje"> No se si quiero estar con él o ser él (no soy furra) <br> Es un labrador y por él me gustan los labradores <br> Es muy carismático y un poco tonto pero tiene buenas intenciones <br> Me gusta mucho como saca su lenguita </div></div>
+                <div class="imagen-personaje"> <div class="personaje-individual"><img src="..\..\statics\media\img\mr_peanutbutter.png" height="250"> </div></div>
+            </div>
+        </div>
+    </section>
 
-    <br><br><br>
-
-    <font size="6" color="#224042"><b> Mis personajes favoritos </b></font> <br><br><br>
-
-    <!--
-    tabla de mis personajes favoritos
-    con:
-    - nombre
-    - porque
-    - foto
-    -->
-
-
-    <table border="1" cellspacing="0">
-        <tr align="center">
-            
-            <td width="255" height="45"> <b> Nombre </b> </td>
-            <td width="350" height="45"> <b> Porque </b> </td>
-            <td width="460" height="45"> <b> Foto </b> </td>
-            
-        </tr>
-        <tr align="center">
-            <td> <b>Haruhi Fujioka</b> <br> de Ouran High School Host Club </td>
-            <td> <p>Es hermosa y linda y amable e inteligente</p> 
-                <p>Haruhi cásate conmigo</p></td>
-            <td align="center" valign="middle" height="250"> <img src="..\..\statics\media\img\haruhi.png" width="460"> </td>
-        </tr>
-        <tr align="center">
-            <td> <b>Kel</b> <br> de Omori </td>
-            <td> Que lindo que es por dios 
-                <p> Otro personaje ficticio que me gustaría tener en mi vida </p>
-                <p> Es muy atletico y le gusta el basketball </p>
-                <p> Me gusta su corte de pelo </p>
-            </td>
-            <td align="center" valign="middle" height="250"> <img src="..\..\statics\media\img\kel.png" width="350"> </td>
-        </tr>
-        <tr align="center">
-            <td> <b>Waymond Wang</b> <br> de Everything Everywhere All At Once </td>
-            <td> Por el todavía creo en la humanidad 
-                <p> Que lindo ser humano de verdad que sí </p>
-                <p> Pasa desapercibido pero es de los personajes más importantes y significativos de la película </p>
-                <p> La verdad, soy yo </p>
-            <td align="center" valign="middle" height="250"> <img src="..\..\statics\media\img\waymond.png" width="430"> </td>
-        </tr>
-        <tr align="center">
-            <td> <b>Kaworu Nagisa</b> <br> de Neon Genesis Evangelion </td>
-            <td> Es un literalmente un ser perfecto
-                <p> Amo su cabello gris parece un viejito y si le queda por que es muy sabio </p>
-                <p> Quiero que alguien me quiera como el quiere a Shinji </p>
-                <p> Te amo Kaworu </p>
-            </td>
-            <td align="center" valign="middle" height="250"> <img src="..\..\statics\media\img\kaworu.png" width="285"> </td>
-        </tr>
-        <tr align="center">
-            <td> <b>Mr. Peanutbutter</b> <br> de Bojack Horseman </td>
-            <td> No se si quiero estar con él o ser él (no soy furra)
-                <p> Es un labrador y por él me gustan los labradores </p>
-                <p> Es muy carismático y un poco tonto pero tiene buenas intenciones </p>
-                <p> Me gusta mucho como saca su lenguita </p> 
-            </td>
-            <td align="center" valign="middle" height="250"> <img src="..\..\statics\media\img\mr_peanutbutter.png" width="330"> </td>
-        </tr>
+    <section id="huevo">
+        <div class="sub_centro"> Un huevo </div>
+        <div class="boton"> <div id="boton_chico"> </div> </div>
         
-    </table>
+        <p align="center"> (perdon era la una de la mañana y no quería hacer nada más complejo) <br><br></p>
+    <section>
 
     </font>
 </body>
